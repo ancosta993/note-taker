5510c9ba21a059436437e9e16caa37ba07f7512b
Anthony Dcosta <adcosta2000@gmail.com>
2022-06-07 01:31:48 -0400

    added POST route

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json')
+const notes = require('./db/db.json');
+const fs = require('fs');
 
 const app = express();
 
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 // static files
 app.use(express.static('public'));
 
@@ -13,6 +19,13 @@ app.get('/api/notes', (req, res) => {
 })
 
 //add a post request route
+app.post('/api/notes', (req, res) => {
+   req.body.id = notes.length.toString();
+   notes.push(req.body);
+   fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify({ note: notes }, null, 2));
+   res.json(notes);
+   res.json(req.body);
+});
 
 // send notes.html
 app.get('/notes', (req, res) => {
